06fdbcbe54543b865952eeeb1757cda476c2bcf2
Brett Turing <bjsherman80@gmail.com>
2020-10-23 13:35:30 -0600

    remove add_ride action and added repairs controller w/ create action to make routes restful

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,18 +6,5 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
-
-  def add_ride
-    if Ride.exists?(id: params[:ride_id])
-      ride = Ride.find_by(id: params[:ride_id])
-      mechanic= Mechanic.find_by(id: params[:id])
-      mechanic.rides << ride
-      redirect_to "/mechanics/#{mechanic.id}"
-    else
-      flash[:notice] = 'Ride does not exist.'
-    end
-  end
-
-
-
+  
 end
