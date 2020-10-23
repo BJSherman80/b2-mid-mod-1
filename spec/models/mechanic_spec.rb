c8c1443d432cfcdc3121f558e1bb9ca3c35cff69
Brett Turing <bjsherman80@gmail.com>
2020-10-23 11:53:51 -0600

    adeed model method for sorted_rides method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :repairs}
     it { should have_many(:rides).through(:repairs)}
   end
+
+  describe 'instance methods' do
+    it 'sorted_rides' do
+      park = Park.create!(name: "Jurassic Park", admission_price: 500)
+      ride_1 = park.rides.create!(name: "T-rex Race", rating: 4)
+      ride_2 = park.rides.create!(name: "Raptor Race", rating: 8)
+      ride_3 = park.rides.create!(name: "Megladon Swim", rating: 10)
+      mechanic_1 = Mechanic.create!(name: "Hank", experience: 10)
+
+      mechanic_1.rides << ride_1
+      mechanic_1.rides << ride_2
+
+      expect(mechanic_1.sorted_rides).to eq([ride_2, ride_1])
+    end
+  end
 end
